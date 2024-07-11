349bd3e1d32e23b7259a726e5d40a563dd9b6bd0
10-ui <ui-10@outlook.jp>
2024-07-11 14:48:48 +0900

    font周りの整備

---
+++ lib/fonts.ts
@@ -0,0 +1,43 @@
+import { Noto_Sans_JP, Red_Hat_Display } from "next/font/google";
+import local from "next/font/local";
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+  variable: "--font-noto-sans-jp",
+});
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"],
+  variable: "--font-red-hat-display",
+});
+
+const lineFont = local({
+  src: [
+    {
+      path: "font/LINESeedJP_OTF_Th.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "font/LINESeedJP_OTF_Rg.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "font/LINESeedJP_OTF_Bd.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "font/LINESeedJP_OTF_Eb.woff2",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+});
+
+export const fonts = {
+  notoSansJP,
+  redHatDisplay,
+  lineFont,
+};
